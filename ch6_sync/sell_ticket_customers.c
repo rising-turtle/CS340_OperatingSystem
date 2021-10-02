a25e1305eb21848213e9a83cf1cf9ff7b626105a
He <hxzhang1@ualr.edu>
2021-10-02 17:40:28 -0400

    make sell_ticket_customer work

--- ch6_sync/sell_ticket_customers.c
+++ ch6_sync/sell_ticket_customers.c
@@ -9,8 +9,9 @@
 
 #define NUM_TICK_SELLER 3 // Number of seller threads 
 #define NUM_SEATS 4	// Number of waiting seats 
-#define NUM_CUSTOMERS 10 // Number of customers to buy tickets 
-#define MAX_SLEEP_SECONDS 2 // Maximum walking (sleeping) seconds 
+#define NUM_CUSTOMERS 20 // Number of customers to buy tickets 
+#define MAX_SLEEP_MILLI_SECONDS 2000 // Maximum walking (sleeping) milliseconds 
+#define MAX_REST_MILLI_SECONDS 500 // Maximum resting milliseconds 
 
 sem_t waiting_customers;
 sem_t ready_sellers;  
@@ -22,31 +23,25 @@ bool quit = false;
 int n_empty_seats = NUM_SEATS ;
 int n_ticket = 100;  // shared between threads 
 
-void random_walk(); // randomly sleep several seconds 
+void random_walk(); // randomly sleep several milliseconds 
+void random_rest(); // randomly rest several milliseconds 
 
 void* seller(void* param) // thread function 
 {
-	while(!quit){
+	while(1){
 
-		// wait here if there is no customer is waiting 
+		// wait here if there is no waiting customer 
 		sem_wait(&waiting_customers); 
-		
+
 		// serve a waiting customer and release the seat 
 		pthread_mutex_lock(&seat_lock); 
-		if(n_empty_seats < NUM_SEATS){
 			++n_empty_seats; // release a customer from seat
 			sem_post(&ready_sellers); // I am ready to serve 
-		}
 		pthread_mutex_unlock(&seat_lock); 
 
-		// check ticket 
-		pthread_mutex_lock(&ticket_lock); 
-		if(n_ticket == 0){
-			printf("Ticket has been sold out, need to stop\n"); 
-			quit = true; 
-		}
-		
-		pthread_mutex_unlock(&ticket_lock);
+		// take some rest
+		random_rest(); 
+
 	}
 	pthread_exit(0); 
 
@@ -62,31 +57,42 @@ void* buy_ticket(void* param)
 
 		// check the seat 
 		pthread_mutex_lock(&seat_lock); 
-		if(n_empty_seats-- > 0){	// if there is some empty seats 
-			sem_post(&waiting_customers); // take the seat and wait 
+		
+		if(!quit && n_empty_seats > 0){	// if there is some empty seats 
+			printf("there are %d empty seats. I'm customer %d, and I'll take a seat and wait for a seller\n", n_empty_seats, customer_id);
+			n_empty_seats--; // take a seat 
+			sem_post(&waiting_customers); // singal sellers with a new waiting customer  
 			pthread_mutex_unlock(&seat_lock); // release the lock to seat 
-
+			
 			// wait for a seller to purchase a ticket 
-			sem_wait(&ready_sellers); // wait here if no seller is available 
+			sem_wait(&ready_sellers); // wait here if no seller is ready 
 
-			// now a seller is available, move forward to purchase a ticket 
+			// now a seller is ready, move forward to purchase a ticket 
 			pthread_mutex_lock(&ticket_lock); // access ticket 
 			if(n_ticket > 0){
 				printf("I am customer %d, I am buying ticket %d\n", customer_id, n_ticket--); 
 			}else{
 				quit = true;
+				printf("Tickets have been sold out, no need to wait, customer %d quit\n", customer_id);
 			}
 			pthread_mutex_unlock(&ticket_lock); // release lock 
 		}else{
-			printf("I am customer %d, there is no empty seats, I am walking out for some time\n", customer_id); 
+			if(quit){
+				printf("Tickets have been sold out, no need to wait, customer %d quit\n", customer_id);
+			}else{
+				printf("I am customer %d, there is no empty seats, I am walking out for some time\n", customer_id); 
+			}
 			pthread_mutex_unlock(&seat_lock); // release the lock to seat 
 		}
 	}
+	printf("Customer %d quit\n", customer_id);
 	pthread_exit(0); 
 }
 
 
 int main(int argc, char* argv[]){
+
+	srand(time(NULL));
 
 	// initialize semaphore 	
 	sem_init(&waiting_customers, 0, 0); 
@@ -96,22 +102,30 @@ int main(int argc, char* argv[]){
 	pthread_mutex_init(&ticket_lock, NULL);
 	pthread_mutex_init(&seat_lock, NULL);  
 
+	// create sellers 
 	pthread_t seller_tid[NUM_TICK_SELLER]; // thread id 
 	for(int i=0; i<NUM_TICK_SELLER; i++){
 		pthread_create(&seller_tid[i], NULL, seller, NULL); 
 	}
 
+	// create customers 
 	pthread_t customer_tid[NUM_CUSTOMERS]; 
+	int customer_id[NUM_CUSTOMERS]; 
 	for(int i=0; i<NUM_CUSTOMERS; i++){
-		pthread_create(&customer_tid[i], NULL, buy_ticket, &i); 
+		customer_id[i] = i+1;
+		pthread_create(&customer_tid[i], NULL, buy_ticket, &customer_id[i]); 
 	}
 
-	for(int i=0; i<NUM_TICK_SELLER; i++){
-		pthread_join(seller_tid[i], NULL); // wait for the thread to exit 
-	}
+	// wait for customers 
 	for(int i=0; i<NUM_CUSTOMERS; i++){
 		pthread_join(customer_tid[i], NULL); 
 	}
+
+	// cancel the sellers
+	for(int i=0; i<NUM_TICK_SELLER; i++){
+		pthread_cancel(seller_tid[i]);
+	}
+
 	
 	printf("Main: quit! \n"); 
 
@@ -120,10 +134,15 @@ int main(int argc, char* argv[]){
 }
 
 
-
-void random_walk() // randomly sleep several seconds 
+void random_walk() // randomly sleep several milliseconds 
 {	
-	srand(time(NULL)); 
-	int ran_v = rand()%MAX_SLEEP_SECONDS + 1;
-	sleep(ran_v); 
+	int ran_v = rand()%MAX_SLEEP_MILLI_SECONDS + 1;
+	usleep(ran_v*1000); 
+}
+
+
+void random_rest() // randomly rest several milliseconds 
+{	
+	int ran_v = rand()%MAX_REST_MILLI_SECONDS + 1;
+	usleep(ran_v*1000); 
 }
