ec25bdb1424015078db118b9bcc2d3d3613e93d3
He <hxzhang1@ualr.edu>
2021-11-12 12:55:46 -0500

    update popen_demo.c

--- ch3_ipc/popen_demo.c
+++ ch3_ipc/popen_demo.c
@@ -4,15 +4,23 @@
 #include <sys/types.h>
 #include <stdlib.h>
 
+char * default_cmd = "ps -eo user,pid,ppid,nlwp,pcpu,pmem,comm";
+
 int main(int argc, char* argv[]){
 
 	char data[1024]; 
+	FILE* pipe; 
 	if(argc <= 1){
-		printf("useage: input a command\n"); 
-		return -1; 
+		// printf("useage: input a command\n"); 
+		printf("use default cmd: %s\n", default_cmd); 
+		pipe = popen(default_cmd, "r"); 
+
+	}else{
+		printf("run cmd: %s\n", argv[1]);
+		pipe = popen(argv[1], "r"); 
 	}
 
-	FILE* pipe = popen(argv[1], "r"); 
+	// FILE* pipe = popen(argv[1], "r"); 
 	int line = 1; 
 	while(fgets(data, sizeof(data), pipe) != NULL){
 
