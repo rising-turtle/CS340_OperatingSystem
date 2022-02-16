d488821df1e730fe3fbcec1b470b8791128eb0c3
He Zhang <fuyinzh@gmail.com>
2022-02-16 14:56:31 -0500

    Create thread_quiz.c

---
+++ ch4_threads/thread_quiz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+
+void *runner();
+
+int main(){
+   pthread_t tid; 
+   pthread_create(&tid,NULL,runner,NULL);
+   printf("Hello process %d. \n", getpid());
+   pthread_join(tid,NULL);
+   printf("Hello process %d. \n", getpid());
+}
+
+
+void *runner(){
+   printf("Hello thread.\n");
+   char* args[3];
+   args[0] ="/usr/bin/ls";
+   args[1] = "-a";
+   args[2] = NULL;
+   execv(args[0],args);
+   pthread_exit(0);
+}
