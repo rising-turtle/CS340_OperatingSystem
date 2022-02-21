1f5510ec3bcec6617dc918ac8b2de97fece84628
He <hxzhang1@ualr.edu>
2022-02-21 12:38:34 -0500

    update

--- ch4_threads/thread_quiz.c
+++ ch4_threads/thread_quiz.c
@@ -8,16 +8,16 @@ void *runner();
 int main(){
    pthread_t tid; 
    pthread_create(&tid,NULL,runner,NULL);
-   printf("Hello process %d. \n", getpid());
+   printf("Hello world %d. \n", getpid());
    pthread_join(tid,NULL);
-   printf("Hello process %d. \n", getpid());
+   printf("Hello world %d. \n", getpid());
 }
 
 
 void *runner(){
    printf("Hello thread.\n");
    char* args[3];
-   args[0] ="/usr/bin/ls";
+   args[0] ="/bin/ls";
    args[1] = "-a";
    args[2] = NULL;
    execv(args[0],args);
